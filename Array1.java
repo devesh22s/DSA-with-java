becd5a8f3b381fe660eb28e9b750e24e1acf7fe6
devesh22s <devesh262004@gmail.com>
2025-05-20 15:02:18 +0530

    arrays

---
+++ Array1.java
@@ -0,0 +1,19 @@
+public class Array1{
+    public static void main(String[] args) {
+
+        //  arrays -> it is the collection of elements of the same data type
+        int [] arr = new int[5]; // initialization
+        int [] arr1 = {1,12,3,4};  // initialization + declaration
+        System.out.println(arr);
+
+
+        //  for each loop - used in collection of elements
+        for(int i : arr1){
+            System.out.println(i);
+        }
+
+
+
+    
+}
+}
